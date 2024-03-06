513d427dce384299fbc86989a196fa8afc3f1547
C4GH <caghclapton@gmail.com>
2024-03-05 18:43:07 -0600

    cambio de text_preprocessor para trabajarlo como una clase.

--- vocab_emb_v2.py
+++ vocab_emb_v2.py
@@ -1,24 +1,26 @@
 import numpy as np
 import torch
+import os
 
 # Cargar el contenido del archivo
-with open('../texto/vectors.txt', 'rt') as fi:
+with open('C:\\Users\\afloresre\\Documents\\Cagh\\Red\\vectors_test.txt', 'rt') as fi:
+    # Procesa el archivo aquí
     contenido = [line.strip().split(' ') for line in fi.readlines()]
 
 # Separar palabras y embeddings
 vocab, embeddings = zip(*[(line[0], list(map(float, line[1:]))) for line in contenido])
 
 # Convertir a numpy arrays
-vocab_npa = np.arra(vocab, dtype='<U10') # ajuste del tipo de datos.
-ems_npa = np.array(embeddings)
+vocab_npa = np.array(vocab, dtype='<U10') # ajuste del tipo de datos.
+embs_npa = np.array(embeddings)
 
 # Insertar tokens especiales
 vocab_npa = np.insert(vocab_npa, 0, ['<pad>', '<sep>'])
 pad_emb_npa = np.zeros((1, embs_npa.shape[1])) # Embedding para '<pad>'
-sep_emd_npa = np.mean(ems_npa, axis=0, keepdims=True) # Embedding promedio para '<sep>'
+sep_emd_npa = np.mean(embs_npa, axis=0, keepdims=True) # Embedding promedio para '<sep>'
 
 # Añadir embeddings para tokens especiales
-ems_npa =np.vstack((pad_emb_npa, sep_emd_npa, ems_npa))
+embs_npa =np.vstack((pad_emb_npa, sep_emd_npa, embs_npa))
 
 # Crea la capa de embedding
 my_embedding_layer = torch.nn.Embedding.from_pretrained(torch.from_numpy(embs_npa).float())
@@ -27,6 +29,8 @@ my_embedding_layer = torch.nn.Embedding.from_pretrained(torch.from_numpy(embs_np
 assert my_embedding_layer.weight.shape == embs_npa.shape
 
 # guardar vocabulario y embeddings
-np.save('./salda/vocab_npa.npy', vocab_npa)
-np.save('./salda/embs_npa.npy', embs_npa)
+if not os.path.exists('./salida'):
+    os.makedirs('./salida')
+np.save('./salida/vocab_npa.npy', vocab_npa)
+np.save('./salida/embs_npa.npy', embs_npa)
 
