5f0006c0bf87970c04865b2c348c3537bccab3b5
C4GH <caghclapton@gmail.com>
2024-03-25 18:28:25 -0600

    Nueva versión con número de parámetros

--- modelo/Modelo2.py
+++ modelo/Modelo2.py
@@ -6,7 +6,7 @@ from modelo import PositionalEncoding as pose
 class Modelo(nn.Module):
     def __init__(self, d_model, nhead, num_layers, num_classes, dropout=0.05):
         super(Modelo, self).__init__()
-        self.positional_encoding = pose(d_model, dropout)
+        self.positional_encoding = pose.PositionalEncoding(d_model, dropout)
         encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dropout=dropout)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
         self.output_layer = nn.Linear(d_model, num_classes)
@@ -18,10 +18,18 @@ class Modelo(nn.Module):
         output = self.output_layer(output)
         return F.log_softmax(output, dim=1)
 
-# Ejemplo de uso
-if __name__ == "__main__":
-    num_classes = 10 # Por ajustar
-    modelo = Modelo(d_model=200, nhead=4, num_layers=3, num_classes=num_classes)
-    src = torch.rand(32, 10, 100) # Suponiendo un batch_size=32, seq_length=10, d_model=100
-    out = modelo(src)
-    print(out) # salida del modelo
+# Configuración del modelo
+d_model = 100 # Debe ser igual al embedding
+nhead = 4
+num_layers = 3
+num_classes = 10 # Asignado arbitrariamente
+dropout = 0.05
+
+# Instanciación del demolo
+modelo = Modelo(d_model=d_model, nhead=nhead, num_layers=num_layers, num_classes=num_classes, dropout=dropout)
+
+# Cálculo del total de parámetros entrenables en el modelo
+total_params = sum(p.numel() for p in modelo.parameters() if p.requires_grad)
+print(f"Total de parámetros entrenables en el modelo: {total_params}")
+
+# Total fue de 1358654
